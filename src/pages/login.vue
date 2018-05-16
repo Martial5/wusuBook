2b5e208ca87b9f9a699fc71c3995909a6bf8cdab
xy <964720401@qq.com>
2018-05-16 16:16:29 +0800

    login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,97 +1,101 @@
 <template>
-    <div >
-        <h1>我是login</h1>
-        <form>
-          <!-- <div class="ipunt-wrap" v-show="admin">
-            <label for="ID" class="icon-id"></label>
-            <input type="text" id="ID" placeholder="账号" v-model="userid">
-          </div> -->
-          <div class="ipunt-wrap">
-            <label for="username" class="icon-user"></label>
-            <input type="text" id="username" placeholder="用户名" v-model="username">
-          </div>
-          <div class="ipunt-wrap">
-            <label for="password" class="icon-password"></label>
-            <input type="password" id="password" placeholder="密码" v-model="password">
-          </div>
-          <div class="button">
-            <a class="gv" href="javascript:;" @click="Login()">登录</a>
-          </div>
-          <!-- <div class="toregist" v-show="customer">
-            还没有账号？<router-link to="/regist"><a href="javascript:;">去注册</a></router-link>
-          </div> -->
-        </form>
-    </div>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">系统登录</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
 </template>
 
-<script type="text/javascript">
-    export default {
-        name: 'login',
-    props: {
-
-		login: {
-			type: Boolean
-		},
-		admin: {
-			type: Boolean
-		},
-		customer: {
-			type: Boolean
-		}
-	},
-	data() {
-		return {
-			username: '',
-			password: '',
-			userid: '',
-			dialog: false,
-			dialogMsg: ''
-		}
-	},
-
-	methods: {
-		confirm() {
-			this.dialog = false
-		},
-		back() {
-			this.$emit('back')
-		},
-		Login() {
-				if (!this.username || !this.password) {
-					alert('请填写完整')
-          return
-				}
-				var user = {
-					username: this.username,
-					password: this.password
-				}
-				$.ajax({
-					url: '/#/login',
-					type: 'post',
-					dataType: 'json',
-					data: user,
-					success: (res) => {
-						console.log(res)
-						if (res.success) {
-							this.username = ''
-							this.password = ''
-							sessionStorage.id = res.data.uid
-							sessionStorage.name = res.data.username
-							sessionStorage.phone = res.data.phone
-							this.$router.push('/matters')
-						} else {
-							alert('用户名或密码出错')
-						}
-					}
-				});
-
-
-		}
-	}
-}
-
+<script>
+  import { requestLogin } from '../api/api';
+  //import NProgress from 'nprogress'
+  export default {
+    data() {
+      return {
+        logining: false,
+        ruleForm2: {
+          account: 'admin',
+          checkPass: '123456'
+        },
+        rules2: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            //{ validator: validaePass }
+          ],
+          checkPass: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            //{ validator: validaePass2 }
+          ]
+        },
+        checked: true
+      };
+    },
+    methods: {
+      handleReset2() {
+        this.$refs.ruleForm2.resetFields();
+      },
+      handleSubmit2(ev) {
+        var _this = this;
+        this.$refs.ruleForm2.validate((valid) => {
+          if (valid) {
+            //_this.$router.replace('/table');
+            this.logining = true;
+            //NProgress.start();
+            var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
+            requestLogin(loginParams).then(data => {
+              this.logining = false;
+              //NProgress.done();
+              let { msg, code, user } = data;
+              if (code !== 200) {
+                this.$message({
+                  message: msg,
+                  type: 'error'
+                });
+              } else {
+                sessionStorage.setItem('user', JSON.stringify(user));
+                this.$router.push({ path: '/table' });
+              }
+            });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+  }
 </style>
